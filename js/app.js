61779fc75ff9b6849a53a9d3f25dc1bb8da2c3f7
satinder <satindersingh472@gmail.com>
2022-07-28 18:59:00 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -10,10 +10,29 @@ function success_function(response){
     }
 }
 function failure_function(error){
-box[`innerHTML`] = `<p>here is some error in getting photos try again </p>`;
+box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
 }
 axios.request({
     url: `https://www.themealdb.com/api/json/v1/1/random.php`
 }).then(success_function).catch(failure_function);
+
+function chicken_success(response){
+for (let i =0; i<response[`data`][`meals`].length; i++){
+    box[`innerHTML`] +=`<h1> Below is the chicken based meal </h1>`;
+    box[`innerHTML`] += `<h2>Name: ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
+    box[`innerHTML`] += `<img src="${response[`data`][`meals`][i][`strMealThumb`]}"/>`;
+}
+}
+function chicken_failure(error){
+    box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
+
+}
+
+axios.request({
+    url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+    params: {
+        c: `chicken`
+    }
+}).then(chicken_success).catch(chicken_failure);
 
 let box= document.getElementById(`box`);
