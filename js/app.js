e816209530f5bceb1686b19447cad674f865730f
satinder <satindersingh472@gmail.com>
2022-07-28 19:00:46 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@ axios.request({
 }).then(success_function).catch(failure_function);
 
 function chicken_success(response){
-for (let i =0; i<response[`data`][`meals`].length; i++){
-    box[`innerHTML`] +=`<h1> Below is the chicken based meal </h1>`;
+    box[`innerHTML`] +=`<h1> Below are the chicken based meal </h1>`;
+    for (let i =0; i<response[`data`][`meals`].length; i++){
     box[`innerHTML`] += `<h2>Name: ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
     box[`innerHTML`] += `<img src="${response[`data`][`meals`][i][`strMealThumb`]}"/>`;
 }
