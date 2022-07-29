ba8a99d54d5af551540f97c839f2beb1a25e7de8
satinder <satindersingh472@gmail.com>
2022-07-28 18:34:59 -0600

    finished'

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function success_function(response){
+    box[`innerHTML`] += `<h1> Below is the Random meal</h1>`;
+    for(let i = 0; i < response[`data`][`meals`].length; i++){
+      box[`innerHTML`] += `<h2 id="place">This is ${response[`data`][`meals`][i][`strArea`]} dish</h1>`;
+      box[`innerHTML`] += `<p id="name">The name of the dish is ${response[`data`][`meals`][i][`strMeal`]}</p>`;
+      box[`innerHTML`] += `<h2 id="heading_instructions"> Step by step instructions to make ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
+      box[`innerHTML`] += `<blockquote id="instruction"> <h3>${response[`data`][`meals`][i][`strInstructions`]}</h3></blockquote>`;
+
+    }
+}
+function failure_function(error){
+box[`innerHTML`] = `<p>There is some error in getting photos</p>`;
+}
+axios.request({
+    url: `https://www.themealdb.com/api/json/v1/1/random.php`
+}).then(success_function).catch(failure_function);
+
+let box= document.getElementById(`box`);
