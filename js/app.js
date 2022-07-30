c327eb6f1e1e535a4217841e3ccb7553ebe69710
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-07-29 18:17:26 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,40 +1,61 @@
-function success_function(response){
+function success_function(response) {
 
-    for(let i = 0; i < response[`data`][`meals`].length; i++){
+    for (let i = 0; i < response[`data`][`meals`].length; i++) {
         box[`innerHTML`] += `<h1> Below is the Random meal</h1>`;
-      box[`innerHTML`] += `<h2 id="place">This is ${response[`data`][`meals`][i][`strArea`]} dish</h1>`;
-      box[`innerHTML`] += `<p id="name">The name of the dish is ${response[`data`][`meals`][i][`strMeal`]}</p>`;
-      box[`innerHTML`] += `<h2 id="heading_instructions"> Step by step instructions to make ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
-      box[`innerHTML`] += `<blockquote id="instruction"> <h3>${response[`data`][`meals`][i][`strInstructions`]}</h3></blockquote>`;
+        box[`innerHTML`] += `<h2 id="place">This is ${response[`data`][`meals`][i][`strArea`]} dish</h1>`;
+        box[`innerHTML`] += `<p id="name">The name of the dish is ${response[`data`][`meals`][i][`strMeal`]}</p>`;
+        box[`innerHTML`] += `<h2 id="heading_instructions"> Step by step instructions to make ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
+        box[`innerHTML`] += `<blockquote id="instruction"> <h3>${response[`data`][`meals`][i][`strInstructions`]}</h3></blockquote>`;
 
     }
 }
-function failure_function(error){
-box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
+function failure_function(error) {
+    box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
 }
 axios.request({
     url: `https://www.themealdb.com/api/json/v1/1/random.php`
 }).then(success_function).catch(failure_function);
 
-function chicken_success(response){
-    box[`innerHTML`] +=`<h1> Below are the chicken based meal </h1>`;
-    for (let i =0; i<response[`data`][`meals`].length; i++){
-    box[`innerHTML`] += `<div id="chicken_box"> <h2>Name: ${response[`data`][`meals`][i][`strMeal`]}</h2> 
+function chicken_success(response) {
+    box[`innerHTML`] += `<h1> Below are the chicken based meal </h1>`;
+    for (let i = 0; i < response[`data`][`meals`].length; i++) {
+        box[`innerHTML`] += `<div id="chicken_box"> <h2>Name: ${response[`data`][`meals`][i][`strMeal`]}</h2> 
     <img src="${response[`data`][`meals`][i][`strMealThumb`]}"/>
     </div>
     `;
+    }
 }
-}
-function chicken_failure(error){
+function chicken_failure(error) {
     box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
 
 }
+function bring_chicken(details) {
+    axios.request({
+        url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+        params: {
+            c: `chicken`
+        }
+    }).then(chicken_success).catch(chicken_failure);
+}
+let box = document.getElementById(`box`);
+let bring_chick = document.getElementById(`bring_chick`)
+bring_chick.addEventListener(`click`,bring_chicken);
 
-axios.request({
-    url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
-    params: {
-        c: `chicken`
+function success_indian(response){
+    for(let i = 0; i<response[`data`][`meals`].length;i++){
+        box[`innerHTML`] += `<h2>Name: ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
     }
-}).then(chicken_success).catch(chicken_failure);
-
-let box= document.getElementById(`box`);
+}
+function failure_indian(error){
+box[`innerHTML`] += `<h2>error</h2>`;
+}
+function indian_food(details){
+    axios.request({
+        url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+        params: {
+            a: `Indian`
+        }
+    }).then(success_indian).catch(failure_indian);
+}
+let indian = document.getElementById(`indian`);
+indian.addEventListener(`click`,indian_food);
