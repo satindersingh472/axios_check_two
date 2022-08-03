61e6674d56ac0ac3b880bfcc5fd653b63efcd725
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-08-03 17:36:29 -0600

    working

--- js/app.js
+++ js/app.js
@@ -21,7 +21,6 @@ function success_categories(response) {
     for (let i = 0; i < response[`data`][`meals`].length; i++) {
         box_category[`innerHTML`] += `<button class="item" id="button_${response[`data`][`meals`][i][`strCategory`]}">${response[`data`][`meals`][i][`strCategory`]}</button`;
     }
-
 }
 function failure_categories(error) {
     box_category[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
@@ -33,18 +32,17 @@ axios.request({
     }
 }).then(success_categories).catch(failure_categories);
 
-
 function success_items(response) {
-
-}
-function failure_items(error) {
-    box_items[`innerHTML`] = `<h2>An error occurred</h2>`;
-}
-function get_items(details) {
-    axios.request({
-        url: `https://themealdb.com/api/json/v1/1/random.php`
-    }).then(success_items).catch(failure_items);
-}
+    box_items[`innerHTML`] += `success`;
+    }
+    function failure_items(error) {
+        box_items[`innerHTML`] = `<h2>An error occurred</h2>`;
+    }
+    function get_items(details) {
+        axios.request({
+            url: `https://www.themealdb.com/api/json/v1/1/filter.php`
+        }).then(success_items).catch(failure_items);
+    }
 
 let item = document.getElementsByClassName(`item`);
 for (let i = 0; i < item.length; i++) {
@@ -52,6 +50,3 @@ item[i].addEventListener(`click`,get_items);
 }
 let box_category = document.getElementById(`box_categories`);
 let box_items = document.getElementById(`box_items`);
-
-
-
