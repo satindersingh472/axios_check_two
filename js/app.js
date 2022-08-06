95a05b8a791828db447b1c332a746840b59c2300
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-08-06 01:56:07 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
 
-// function success_function(response){
-
-//     for(let i = 0; i < response[`data`][`meals`].length; i++){
-//         box[`innerHTML`] += `<h1> Below is the Random meal</h1>`;
-//       box[`innerHTML`] += `<h2 id="place">This is ${response[`data`][`meals`][i][`strArea`]} dish</h1>`;
-//       box[`innerHTML`] += `<p id="name">The name of the dish is ${response[`data`][`meals`][i][`strMeal`]}</p>`;
-//       box[`innerHTML`] += `<h2 id="heading_instructions"> Step by step instructions to make ${response[`data`][`meals`][i][`strMeal`]}</h2>`;
-//       box[`innerHTML`] += `<blockquote id="instruction"> <h3>${response[`data`][`meals`][i][`strInstructions`]}</h3></blockquote>`;
-
-//     }
-// }
-// function failure_function(error){
-// box[`innerHTML`] = `<p>here is some error in getting information try again </p>`;
-// }
-// axios.request({
-//     url: `https://www.themealdb.com/api/json/v1/1/random.php`
-// }).then(success_function).catch(failure_function)
-
 function success_categories(response) {
     for (let i = 0; i < response[`data`][`meals`].length; i++) {
         box_category[`innerHTML`] += `<button class="item" id="button_${response[`data`][`meals`][i][`strArea`]}">${response[`data`][`meals`][i][`strArea`]}</button`;
@@ -33,13 +15,13 @@ axios.request({
 }).then(success_categories).catch(failure_categories);
 
 function success_items(response) {
-    location.href = `index.html`;
-    let meals = response[`data`][`meals`];
-    let meals_data = JSON.stringify(meals);
-    Cookies.set(`category_items`, meals_data);
+    box_items[`innerHTML`] = ``;
+    for (let i = 0; i < response[`data`][`meals`].length; i++) {
+        box_item[`innerHTML`] += `<div>${response[`data`][`meals`][i][`strMeal`]}</div>`;
+    }
 }
 function failure_items(error) {
-    box_items[`innerHTML`] = `<h2>An error occurred</h2>`;
+    box_item[`innerHTML`] = `<h2>An error occurred</h2>`;
 }
 function get_items(details) {
     let value_c = details[`target`][`innerHTML`];
@@ -50,17 +32,64 @@ function get_items(details) {
         }
     }).then(success_items).catch(failure_items);
 }
+function success_id(response) {
+    let obj = response[`data`][`meals`][0];
+    box_item[`innerHTML`] = ``;
+    box_item[`innerHTML`] = `<div class="ingredient">
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient1`]}.png"/> 
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient2`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient3`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient4`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient5`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient6`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient7`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient8`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient9`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient10`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient11`]}.png"/> 
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient12`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient13`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient14`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient15`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient16`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient17`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient18`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient19`]}.png"/>
+<img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient20`]}.png"/>
+</div>`
+;
+}
+function failed_id(error) {
 
-let box_button = document.getElementById(`box_categories`);
-box_button.addEventListener(`click`, get_items);
-
-let box_category = document.getElementById(`box_categories`);
-let box_items = document.getElementById(`box_items`);
-
-let cookies_value = Cookies.get(`category_items`);
-if (cookies_value) {
-    let data = JSON.parse(cookies_value);
-    for (let i = 0; i < data.length; i++) {
-        box_items[`innerHTML`] += `<div>${data[i][`strMeal`]}</div>`;
+}
+function success_details(response) {
+    let id_value;
+    for (let i = 0; i < response[`data`][`meals`].length; i++) {
+        id_value = response[`data`][`meals`][i][`idMeal`];
+        axios.request({
+            url: `https://www.themealdb.com/api/json/v1/1/lookup.php`,
+            params: {
+                i: id_value
+            }
+        }).then(success_id).then(failed_id);
     }
 }
+function failure_details(error) {
+    box[`innerHTML`] = `There is an error`;
+}
+function get_details(details) {
+    let name_value = details[`target`][`innerHTML`];
+    axios.request({
+        url: `https://www.themealdb.com/api/json/v1/1/search.php`,
+        params: {
+            s: name_value
+        }
+    }).then(success_details).catch(failure_details);
+}
+
+
+let box_category = document.getElementById(`box_categories`);
+box_category.addEventListener(`click`, get_items);
+
+let box_item = document.getElementById(`box_items`);
+box_item.addEventListener(`click`, get_details);
