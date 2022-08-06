a13aca0506f4499405b522146744091e168e64bd
satinder <satindersingh472@gmail.com>
2022-08-06 09:02:04 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ axios.request({
 }).then(success_categories).catch(failure_categories);
 
 function success_items(response) {
-    box_items[`innerHTML`] = ``;
+    box_item[`innerHTML`] = ``;
     for (let i = 0; i < response[`data`][`meals`].length; i++) {
         box_item[`innerHTML`] += `<div>${response[`data`][`meals`][i][`strMeal`]}</div>`;
     }
@@ -32,6 +32,9 @@ function get_items(details) {
         }
     }).then(success_items).catch(failure_items);
 }
+
+
+
 function success_id(response) {
     let obj = response[`data`][`meals`][0];
     box_item[`innerHTML`] = ``;
@@ -56,12 +59,13 @@ function success_id(response) {
 <img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient18`]}.png"/>
 <img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient19`]}.png"/>
 <img class="ingredient_image" src="https://www.themealdb.com/images/ingredients/${obj[`strIngredient20`]}.png"/>
-</div>`
-;
+</div>`;
 }
-function failed_id(error) {
 
+function failed_id(error) {
+    box_item[`innerHTML`] += `there is some error in the process`;
 }
+
 function success_details(response) {
     let id_value;
     for (let i = 0; i < response[`data`][`meals`].length; i++) {
@@ -75,16 +79,18 @@ function success_details(response) {
     }
 }
 function failure_details(error) {
-    box[`innerHTML`] = `There is an error`;
+    box_item[`innerHTML`] = `There is an error in getting details of meals`;
 }
 function get_details(details) {
-    let name_value = details[`target`][`innerHTML`];
-    axios.request({
-        url: `https://www.themealdb.com/api/json/v1/1/search.php`,
-        params: {
-            s: name_value
-        }
-    }).then(success_details).catch(failure_details);
+    if (details[`target`] !== details[`currentTarget`]) {
+        let name_value = details[`target`][`innerHTML`];
+        axios.request({
+            url: `https://www.themealdb.com/api/json/v1/1/search.php`,
+            params: {
+                s: name_value
+            }
+        }).then(success_details).catch(failure_details);
+    }
 }
 
 
